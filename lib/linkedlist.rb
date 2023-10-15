33ac8991b1d7277c585bbf6c9400872301fde055
Dylan Perry <dylanbperry@gmail.com>
2023-10-15 16:12:51 -0700

    Correct append method and tests; was previously prepending

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -9,12 +9,14 @@ class LinkedList
 
     def append(node_data)
         if @head
-            current_head = @head
-            @head = Node.new(node_data)
-            head.next_node = current_head
+            current_node = @head
+            until current_node.next_node == nil
+                current_node = current_node.next_node
+            end
+            current_node.next_node = Node.new(node_data)
         else
             @head = Node.new(node_data)
-        end
+        end     
     end
 
     def count
@@ -34,15 +36,25 @@ class LinkedList
     def to_string
         final_string = ""
         if !@head
-            final_string = "List contains no data."
+            final_string = "Error: list contains no data."
         else
             current_node = @head
             final_string = current_node.data
             while current_node.next_node != nil
                 current_node = current_node.next_node
-                final_string.prepend("#{current_node.data} ")
+                final_string << " #{current_node.data}"
             end
             final_string
+        end
+    end
+
+    def prepend(node_data)
+        if @head
+            current_head = @head
+            @head = Node.new(node_data)
+            head.next_node = current_head
+        else
+            @head = Node.new(node_data)
         end
     end
 end
--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -16,7 +16,7 @@ describe LinkedList do
         expect(list.head).to eq(nil)
     end
 
-    it "appends a node" do
+    it "appends a node to an empty list" do
         list = LinkedList.new
         list.append("doop")
 
@@ -24,15 +24,15 @@ describe LinkedList do
         expect(list.head.data).to eq("doop")
     end
 
-    it "shifts node to next_node when appending subsequent nodes" do
+    it "appends a node to the end of a list" do
         list = LinkedList.new
         list.append("doop")
         list.append("snoop")
 
         expect(list.head).to be_a Node
-        expect(list.head.data).to eq("snoop")
+        expect(list.head.data).to eq("doop")
         expect(list.head.next_node).to be_a Node
-        expect(list.head.next_node.data).to eq("doop")
+        expect(list.head.next_node.data).to eq("snoop")
     end
 
     it "counts the nodes" do
@@ -52,4 +52,22 @@ describe LinkedList do
 
         expect(list.to_string).to eq("doop snoop blarp")
     end
+
+    it "prints an error when attempting to stringify an empty list" do
+        list = LinkedList.new
+
+        expect(list.to_string).to eq("Error: list contains no data.")
+    end
+
+    # it "prepends the list with a node" do
+    #     list = LinkedList.new
+    #     list.append("doop")
+    #     list.append("snoop")
+
+    #     expect(list.head).to be_a Node
+    #     expect(list.head.data).to eq("snoop")
+    #     expect(list.head.next_node).to be_a Node
+    #     expect(list.head.next_node.data).to eq("doop")
+    # end
+
 end
