a9de4621f69192b05cd7e8b5a87e66717cc8baf3
Dylan Perry <dylanbperry@gmail.com>
2023-10-16 16:52:10 -0700

    Fix an issue in to_string method where node data would concatenate instead of replace

--- lib/linkedlist.rb
+++ lib/linkedlist.rb
@@ -34,17 +34,16 @@ class LinkedList
     end
 
     def to_string
-        final_string = ""
         if !@head
-            final_string = "Error: list contains no data."
+            "Error: list contains no data."
         else
             current_node = @head
-            final_string = current_node.data
+            string = current_node.data
             while current_node.next_node != nil
                 current_node = current_node.next_node
-                final_string << " #{current_node.data}"
+                string = string + " #{current_node.data}"
             end
-            final_string
+            string
         end
     end
 
